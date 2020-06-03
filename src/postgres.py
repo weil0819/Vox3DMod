fcce4b648394fb7850688d96a03168fd21926fb7
Wesley <weil0819@gmail.com>
2020-06-03 19:55:33 +1000

    update postgres

--- src/postgres.py
+++ src/postgres.py
@@ -6,11 +6,12 @@ Func:
 - Connect PostgreSQL Database.
 - Import Data.
 
-
 Usage:
-> python postgres.py --input='YOUR DATA PATH' --dbname='YOUR DATABASE NAME' --host='YOUR HOST ADDRESS' --user='YOUR USER NAME' --pwd='YOUR PASSWORD' --tbname='YOUR TABLE NAME'
+> python postgres.py --input='YOUR DATA PATH' --dbname='YOUR DATABASE NAME' 
+--host='YOUR HOST ADDRESS' --user='YOUR USER NAME' --pwd='YOUR PASSWORD' --tbname='YOUR TABLE NAME'
 e.g.,
-> python postgres.py --input='../bim.xyz' --dbname='voxeldb' --host='localhost' --user='postgres' --pwd='postgres' --tabname='Dalton' 
+> python postgres.py --input=../bim.xyz --dbname=voxeldb --host=localhost 
+--user=postgres --pwd=postgres --tabname=Dalton 
 
 @date: May 28, 2020
 @author: Wesley
@@ -32,7 +33,83 @@ from platform import python_version
 print('Python Version: ', python_version())
 
 
-def create_db(user, password, dbname, tbname, host='localhost'):
+def get_material_path(depth, x, y, z):
+	'''
+	:function:
+		- Calculate material path of a leaf node in an octree
+	:param depth:
+		- Octree depth
+	:param x,y,z:
+		- The x,y,z coordinates
+	'''
+	ocKey = ''
+	for i in range(depth, 0, -1):
+		digit = 0
+		mask = 1 << (i-1)
+		if (x & mask) != 0:
+			digit += 1
+		if (y & mask) != 0:
+			digit += 2
+		if (z & mask) != 0:
+			digit += 4
+		ocKey +=str(digit)
+	return ocKey
+
+
+def translation_and_scaling(xMin, xMax, yMin, yMax, zMin, zMax, depth):
+	'''
+	:function:
+		- Calculate BBox translation and scaling.
+		- The translation is necessary to define the lower left corner of the front of 
+		the bounding box as the origin of the point cloud. 
+		- The scaling is needed so that our code fits to any kind and size of point cloud, 
+		and so that every cell which we define has coordinates represented by integers between 0 and 2n.
+	:param xMin, xMax, yMin, yMax, zMin, zMax:
+		- min and max boundary
+	:param depth:
+		- The Octree depth
+	:return translateX, translateY, translateZ, scale:
+		- translation and scaling
+	'''
+	# Check bounding to calculate scaling factor.				
+	lenX = xMax - xMin
+	lenY = yMax - yMin
+	lenZ = zMax - zMin
+
+	# Putting the BBox min coordinates at 0.
+	if xMin < 0:
+		translateX = abs(xMin)
+	elif (xMin > 0): 
+		translateX = 0 - xMin
+	else:
+		translateX = 0
+
+	if (yMin < 0):
+		translateY = abs(yMin)
+	elif (yMin > 0): 
+		translateY = 0 - yMin
+	else:
+		translateY = 0
+
+	if (zMin < 0):
+		translateZ = abs(zMin)
+	elif (zMin > 0): 
+		translateZ = 0 - zMin
+	else:
+		translateZ = 0
+
+	if (lenX != 2**depth) or (lenY != 2**depth) or (lenZ != 2**depth):
+		# if scaling is required we take the maximum domain of the point cloud for scaling
+		maxDomain = max([lenX, lenY, lenZ])
+		scale = 2**depth / maxDomain
+	else: 
+		# if no scaling is required we set the scale to 1
+		scale = 1
+
+	return translateX, translateY, translateZ, scale
+
+
+def create_db(user, password, dbname, tbname, host):
 	'''
 	:function:
 		- Create a database and tables 
@@ -52,7 +129,8 @@ def create_db(user, password, dbname, tbname, host='localhost'):
 	conn = None
 	try:
 		print('Connecting to the PostgreSQL database ...')
-		conn = psycopg2.connect("host='" + host + "' user='" + user + "' password='" + password + "'")		
+		conn = psycopg2.connect(host=host, user=user, password=password)
+		conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
 		cur = conn.cursor()
 
 		print('PostgreSQL database version:')		# execute a statement
@@ -63,26 +141,32 @@ def create_db(user, password, dbname, tbname, host='localhost'):
 		cur.execute('SELECT datname FROM pg_database;')
 		list_database = cur.fetchall()
 		if (dbname,) in list_database:
-			print("'{}' Database already exists ...".format(dbname))
+			print('{} Database already exists ...'.format(dbname))
 		else:
 			cur.execute('CREATE DATABASE ' + dbname + ';')
 		cur.close()
-		con.close()
+		conn.close()
 
-		conn = psycopg2.connect("host='" + host + "' database='" + dbname + "' user='" + user + "' password='" + password + "'")
+		conn = psycopg2.connect(host=host, database=dbname, user=user, password=password)
 		cur = conn.cursor()
-		cur.execute('CREATE EXTENSION POSTGIS;')
-		cur.execute('CREATE TABLE '+ tbname + ' (voxid SERIAL PRIMARY KEY, x INTEGER NOT NULL, y INTEGER NOT NULL, z INTEGER NOT NULL, objNum INTEGER;')
+		cur.execute('CREATE EXTENSION IF NOT EXISTS POSTGIS;')
+		# cur.execute("select exists(select relname from pg_class where relname='" + tbname + "');")
+		# tb_exist = cur.fetchone()[0]
+		# if tb_exist:
+		# 	print('{} table already exists ...'.format(tbname))
+		# else:
+			# cur.execute('CREATE TABLE IF NOT EXISTS'+ tbname + 
+			# 	' (voxid SERIAL PRIMARY KEY, x INTEGER NOT NULL, y INTEGER NOT NULL, z INTEGER NOT NULL, objNum INTEGER);')
+		cur.execute('CREATE TABLE IF NOT EXISTS '+ tbname + 
+			' (voxid serial PRIMARY KEY, x INTEGER NOT NULL, y INTEGER NOT NULL, z INTEGER NOT NULL, objNum INTEGER, materialpath VARCHAR(100));')
 		cur.close()
 		conn.commit()	# commit the changes		
 	except (Exception, psycopg2.DatabaseError) as error:
 		print(error)
 	finally:
-		# return conn
-		if conn is not None:
-			conn.close()
+		return conn
 
-"""
+
 def write_db(conn, tbname, file):
 	'''
 	:function:
@@ -97,20 +181,59 @@ def write_db(conn, tbname, file):
 	if conn is not None:
 		try:
 			cur = conn.cursor()
+			xyzset = set()		# store xyz in each line from raw data file
+			xMin, yMin, zMin = None, None, None
+			xMax, yMax, zMax = None, None, None
 			with open(file, mode='r', encoding='utf-8') as f:
 				while(True):
 					line = f.readline().strip()
 					if not line:
 						break
-					cur.execute("INSERT INTO " + tbname + " VALUES('{}')".format(line.split()[0], line.split()[1], line.split()[2], line.split()[3]))
-		except (Exception, psycopg2.DatabaseError) as error:
-	        print(error)
-	    finally:
-	        if conn is not None:
-	            conn.close()
-	            print('Database connection closed.')
+					x, y, z, objNum = int(line.split()[0]), int(line.split()[1]), int(line.split()[2]), int(line.split()[3])
+					xyzset.add(tuple(int(i) for i in line.split()))
+					if xMin is None:
+						xMin, yMin, zMin = x, y, z
+						xMax, yMax, zMax = x, y, z
+					if x < xMin:
+						xMin = x
+					if y < yMin:
+						yMin = y
+					if z < zMin:
+						zMin = z
+					if x > xMax:
+						xMax = x
+					if y > yMax:
+						yMax = y
+					if z > zMax:
+						zMax = z
 
-"""
+			depth = 8
+			translateX, translateY, translateZ, scale = translation_and_scaling(xMin, xMax, yMin, yMax, zMin, zMax, depth)
+			for line in xyzset:
+				xRaw, yRaw, zRaw = line[0], line[1], line[2]
+				objNum = line[3]
+				# Scaling coordinates.
+				x = (xRaw + translateX) * scale
+				y = (yRaw + translateY) * scale
+				z = (zRaw + translateZ) * scale
+
+				# Snap point to leaf node by converting float to integer and truncate towards 0.
+				leafNode = (int(x), int(y), int(z))
+
+				# Retrieve material path from box.
+				materialPath = get_material_path(depth, leafNode[0], leafNode[1], leafNode[2])
+
+				cur.execute("INSERT INTO " + tbname + 
+					" (x, y, z, objnum, materialpath) VALUES({0}, {1}, {2}, {3}, {4})".format(
+						int(xRaw), int(yRaw), int(zRaw), int(objNum), str(materialPath)))
+			cur.close()
+			conn.commit()
+		except (Exception, psycopg2.DatabaseError) as error:
+			print(error)
+		finally:
+			conn.close()
+			print('Database connection closed.')
+
 
 if __name__=='__main__':
 	print('********** Initializing ArgumentParser and related arguments **********')
@@ -127,7 +250,7 @@ if __name__=='__main__':
 	conn = create_db(args.user, args.pwd, args.dbname, args.tbname, args.host)
 
 	# ********** Writing Database **********
-	# write_db(conn, args.tbname, args.input)
+	write_db(conn, args.tbname, args.input)
 
 
 
--- src/xyz2oct.py
+++ src/xyz2oct.py
@@ -9,7 +9,7 @@ Usage:
 E.g.,
 > python xyz2oct.py --input='../data/bim/Dalton/classmodel.xyz' --output='../data/bim/Dalton/classmodel.oct'
 
-@date: June 1, 2020
+@date: June 2, 2020
 @author: Wesley
 
 """
@@ -51,10 +51,63 @@ def get_material_path(depth, x, y, z):
 	return ocKey
 
 
+def translation_and_scaling(xMin, xMax, yMin, yMax, zMin, zMax, depth):
+	'''
+	:function:
+		- Calculate BBox translation and scaling.
+		- The translation is necessary to define the lower left corner of the front of 
+		the bounding box as the origin of the point cloud. 
+		- The scaling is needed so that our code fits to any kind and size of point cloud, 
+		and so that every cell which we define has coordinates represented by integers between 0 and 2n.
+	:param xMin, xMax, yMin, yMax, zMin, zMax:
+		- min and max boundary
+	:param depth:
+		- The Octree depth
+	:return translateX, translateY, translateZ, scale:
+		- translation and scaling
+	'''
+	# Check bounding to calculate scaling factor.				
+	lenX = xMax - xMin
+	lenY = yMax - yMin
+	lenZ = zMax - zMin
+
+	# Putting the BBox min coordinates at 0.
+	if xMin < 0:
+		translateX = abs(xMin)
+	elif (xMin > 0): 
+		translateX = 0 - xMin
+	else:
+		translateX = 0
+
+	if (yMin < 0):
+		translateY = abs(yMin)
+	elif (yMin > 0): 
+		translateY = 0 - yMin
+	else:
+		translateY = 0
+
+	if (zMin < 0):
+		translateZ = abs(zMin)
+	elif (zMin > 0): 
+		translateZ = 0 - zMin
+	else:
+		translateZ = 0
+
+	if (lenX != 2**depth) or (lenY != 2**depth) or (lenZ != 2**depth):
+		# if scaling is required we take the maximum domain of the point cloud for scaling
+		maxDomain = max([lenX, lenY, lenZ])
+		scale = 2**depth / maxDomain
+	else: 
+		# if no scaling is required we set the scale to 1
+		scale = 1
+
+	return translateX, translateY, translateZ, scale
+
 
 if __name__=='__main__':
 	print('********** Initializing ArgumentParser and related arguments **********')
-	parser = argparse.ArgumentParser(description='Input and output data path', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser = argparse.ArgumentParser(description='Input and output data path', 
+		formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 	parser.add_argument('--input', help='directory for input data')
 	parser.add_argument('--output', help='directory for output data')
 	args = parser.parse_args(sys.argv[1:])
@@ -63,18 +116,53 @@ if __name__=='__main__':
 
 	print('********** Opening .xyz file **********')
 	xyzset = set()		# store xyz in each line from raw data file
+	xMin, yMin, zMin = None, None, None
+	xMax, yMax, zMax = None, None, None
 	message = 'Opening "{}"'.format(args.input)	# output message
 	with stopwatch(message):
 		with open(args.input, mode='r', encoding='utf-8') as f:
 			while(True):
 				line = f.readline().strip()
 				if not line:
-					break
-				xyzset.add(tuple(int(i) for i in line.split()[:3]))
-
+					break				
+				x, y, z, objNum = int(line.split()[0]), int(line.split()[1]), int(line.split()[2]), int(line.split()[3])
+				xyzset.add(tuple(int(i) for i in line.split()))
+				if xMin is None:
+					xMin, yMin, zMin = x, y, z
+					xMax, yMax, zMax = x, y, z
+				if x < xMin:
+					xMin = x
+				if y < yMin:
+					yMin = y
+				if z < zMin:
+					zMin = z
+				if x > xMax:
+					xMax = x
+				if y > yMax:
+					yMax = y
+				if z > zMax:
+					zMax = z
+				
+	depth = 8
+	translateX, translateY, translateZ, scale = translation_and_scaling(xMin, xMax, yMin, yMax, zMin, zMax, depth)
 	print('********** Writing .obj file **********')
 	message = 'Writing "{}"'.format(args.output)
 	with stopwatch(message):
 		with open(args.output, mode='w', encoding='utf-8') as f:
+			for line in xyzset:
+				xRaw, yRaw, zRaw = line[0], line[1], line[2]
+				objNum = line[3]
+				# Scaling coordinates.
+				x = (xRaw + translateX) * scale
+				y = (yRaw + translateY) * scale
+				z = (zRaw + translateZ) * scale
 
+				# Snap point to leaf node by converting float to integer and truncate towards 0.
+				leafNode = (int(x), int(y), int(z))
+
+				# Retrieve material path from box.
+				materialPath = get_material_path(depth, leafNode[0], leafNode[1], leafNode[2])
+
+				# Write material path into .oct file.
+				f.write(str(xRaw)+' '+str(yRaw)+' '+str(zRaw)+' '+str(objNum)+' '+str(materialPath)+'\n')
 
