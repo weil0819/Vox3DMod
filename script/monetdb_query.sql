dd173c061591537d1a124fd7bce33da5e21fabe6
Wei Li <z5039792@ad.unsw.edu.au>
2020-07-07 18:53:47 +1000

    update query files

--- script/monetdb_query.sql
+++ script/monetdb_query.sql
@@ -1,20 +1,22 @@
-/* ---------------------------------------------------- */
-/*  Created On : 26-June-2020 	             			*/
-/*  DBMS       : MonetDB	 			     			*/
-/*  Usage      : sql>\< C:/monetdb_query.sql            */
-/*  Author     : Wesley      			     			*/
-/* ---------------------------------------------------- */
+/x,y,z ---------------------------------------------------- x,y,z/
+/x,y,z  Created On : 26-June-2020 	             			x,y,z/
+/x,y,z  DBMS       : MonetDB	 			     			x,y,z/
+/x,y,z  Output     : sql>\> C:/monetdb_result.log           x,y,z/
+/x,y,z  Output     : sql>\t performance						x,y,z/
+/x,y,z  Usage      : sql>\< C:/monetdb_query.sql            x,y,z/
+/x,y,z  Author     : Wesley      			     			x,y,z/
+/x,y,z ---------------------------------------------------- x,y,z/
 
 
-\t performance
+-- \t performance
 
 /* Query 1 */
-EXPLAIN SELECT * FROM building WHERE buildID=1;
-EXPLAIN SELECT * FROM building WHERE buildID=1;
-EXPLAIN SELECT * FROM building WHERE buildID=1;
-EXPLAIN SELECT * FROM be;
-EXPLAIN SELECT * FROM be;
-EXPLAIN SELECT * FROM be;
+EXPLAIN SELECT x,y,z FROM building WHERE buildID=1;
+EXPLAIN SELECT x,y,z FROM building WHERE buildID=1;
+EXPLAIN SELECT x,y,z FROM building WHERE buildID=1;
+EXPLAIN SELECT x,y,z FROM be;
+EXPLAIN SELECT x,y,z FROM be;
+EXPLAIN SELECT x,y,z FROM be;
 
 -- DROP INDEX idx_building_ID;
 -- CREATE IMPRINTS INDEX idx_building_ID ON building (buildID);
@@ -22,36 +24,37 @@ EXPLAIN SELECT * FROM be;
 
 
 /* Query 2 */
-EXPLAIN SELECT * FROM building WHERE buildID=1 AND objID=8;
-EXPLAIN SELECT * FROM building WHERE buildID=1 AND objID=8;
-EXPLAIN SELECT * FROM building WHERE buildID=1 AND objID=8;
-EXPLAIN SELECT * FROM be WHERE objID=8;
-EXPLAIN SELECT * FROM be WHERE objID=8;
-EXPLAIN SELECT * FROM be WHERE objID=8;
+EXPLAIN SELECT x,y,z FROM building WHERE buildID=1 AND objID=8;
+EXPLAIN SELECT x,y,z FROM building WHERE buildID=1 AND objID=8;
+EXPLAIN SELECT x,y,z FROM building WHERE buildID=1 AND objID=8;
+EXPLAIN SELECT x,y,z FROM be WHERE objID=8;
+EXPLAIN SELECT x,y,z FROM be WHERE objID=8;
+EXPLAIN SELECT x,y,z FROM be WHERE objID=8;
 
-DROP INDEX idx_be_obj;
-CREATE IMPRINTS INDEX idx_be_obj ON be (objID);
-EXPLAIN SELECT * FROM be WHERE objID=8;
-EXPLAIN SELECT * FROM be WHERE objID=8;
-EXPLAIN SELECT * FROM be WHERE objID=8;
+-- DROP INDEX idx_be_obj;
+-- CREATE IMPRINTS INDEX idx_be_obj ON be (objID);
+-- EXPLAIN SELECT x,y,z FROM be WHERE objID=8;
+-- EXPLAIN SELECT x,y,z FROM be WHERE objID=8;
+-- EXPLAIN SELECT x,y,z FROM be WHERE objID=8;
 
 -- EXPLAIN  SELECT x,y,z FROM building WHERE buildID=1 and objID=5;
 
 /* Query 3 */
-EXPLAIN SELECT * FROM building WHERE buildID=1 AND x > 100 AND x < 500 AND y > 30 AND y < 330;
-EXPLAIN SELECT * FROM building WHERE buildID=1 AND x > 100 AND x < 500 AND y > 30 AND y < 330;
-EXPLAIN SELECT * FROM building WHERE buildID=1 AND x > 100 AND x < 500 AND y > 30 AND y < 330;
-EXPLAIN SELECT * FROM be WHERE x > 100 AND x < 500 AND y > 30 AND y < 330;
-EXPLAIN SELECT * FROM be WHERE x > 100 AND x < 500 AND y > 30 AND y < 330;
-EXPLAIN SELECT * FROM be WHERE x > 100 AND x < 500 AND y > 30 AND y < 330;
+EXPLAIN SELECT x,y,z FROM building WHERE buildID=1 AND x > 100 AND x < 500 AND y > 30 AND y < 330;
+EXPLAIN SELECT x,y,z FROM building WHERE buildID=1 AND x > 100 AND x < 500 AND y > 30 AND y < 330;
+EXPLAIN SELECT x,y,z FROM building WHERE buildID=1 AND x > 100 AND x < 500 AND y > 30 AND y < 330;
+EXPLAIN SELECT x,y,z FROM be WHERE x > 100 AND x < 500 AND y > 30 AND y < 330;
+EXPLAIN SELECT x,y,z FROM be WHERE x > 100 AND x < 500 AND y > 30 AND y < 330;
+EXPLAIN SELECT x,y,z FROM be WHERE x > 100 AND x < 500 AND y > 30 AND y < 330;
+
 
 /* Query 4 */
-EXPLAIN SELECT * FROM building WHERE buildID=1 AND sqrt(power(x-100, 2) + power(y-200, 2)) < 100;
-EXPLAIN SELECT * FROM building WHERE buildID=1 AND sqrt(power(x-100, 2) + power(y-200, 2)) < 100;
-EXPLAIN SELECT * FROM building WHERE buildID=1 AND sqrt(power(x-100, 2) + power(y-200, 2)) < 100;
-EXPLAIN SELECT * FROM be WHERE sqrt(power(x-100, 2) + power(y-200, 2)) < 100;
-EXPLAIN SELECT * FROM be WHERE sqrt(power(x-100, 2) + power(y-200, 2)) < 100;
-EXPLAIN SELECT * FROM be WHERE sqrt(power(x-100, 2) + power(y-200, 2)) < 100;
+EXPLAIN SELECT x,y,z FROM building WHERE buildID=1 AND sqrt(power(x-100, 2) + power(y-200, 2)) < 100;
+EXPLAIN SELECT x,y,z FROM building WHERE buildID=1 AND sqrt(power(x-100, 2) + power(y-200, 2)) < 100;
+EXPLAIN SELECT x,y,z FROM building WHERE buildID=1 AND sqrt(power(x-100, 2) + power(y-200, 2)) < 100;
+EXPLAIN SELECT x,y,z FROM be WHERE sqrt(power(x-100, 2) + power(y-200, 2)) < 100;
+EXPLAIN SELECT x,y,z FROM be WHERE sqrt(power(x-100, 2) + power(y-200, 2)) < 100;
+EXPLAIN SELECT x,y,z FROM be WHERE sqrt(power(x-100, 2) + power(y-200, 2)) < 100;
 
 /* Query 5 */
 -- EXPLAIN  SELECT MAX(z) FROM be WHERE x > 30 AND y < 130 GROUP BY objID;
